7180502f2c5f0981f9a40039444ae41d02177594
Ryan Jay Cuevas <ryan.cuevas@ispsc.edu.ph>
2025-12-19 11:34:28 +0800

    update urls

--- conf/urls.py
+++ conf/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('guestbook/', include('guestbook.urls')),  #guestbook/ - directory for guestbook app
+    path('', include('guestbook.urls'))
 ]
